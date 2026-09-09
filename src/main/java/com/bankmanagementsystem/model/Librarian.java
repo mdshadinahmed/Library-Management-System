4c3df58b5d4a80e7a76e13b7f59f06f4ae36b9f3
Shadin Ahmed <shadinahmed2525@gmail.com>
2026-09-09 15:36:36 +0600

    Add Complete Model class Librarian

--- src/main/java/com/bankmanagementsystem/model/Librarian.java
+++ src/main/java/com/bankmanagementsystem/model/Librarian.java
@@ -1,4 +1,40 @@
 package com.bankmanagementsystem.model;
 
 public class Librarian {
+
+    private int librarianId;
+    private String name;
+
+    public Librarian(){
+        super();
+    }
+
+    public Librarian(int librarianId, String name) {
+        this.librarianId = librarianId;
+        this.name = name;
+    }
+
+    public int getLibrarianId() {
+        return librarianId;
+    }
+
+    public void setLibrarianId(int librarianId) {
+        this.librarianId = librarianId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displayLibrarianInfo() {
+
+        System.out.println("------------------------------");
+        System.out.println("Librarian ID : " + librarianId);
+        System.out.println("Name         : " + name);
+        System.out.println("------------------------------");
+    }
 }
