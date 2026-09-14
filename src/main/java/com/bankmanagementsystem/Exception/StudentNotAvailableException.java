4e9954b1211385f6fb5007b0ecf06df3acfc5fd7
Shadin Ahmed <shadinahmed2525@gmail.com>
2026-09-14 14:28:02 +0600

    Change Excepton Class from StudentNotFoundException to StudentNotAvailableException

--- src/main/java/com/bankmanagementsystem/Exception/StudentNotAvailableException.java
+++ src/main/java/com/bankmanagementsystem/Exception/StudentNotAvailableException.java
@@ -1,7 +1,7 @@
 package com.bankmanagementsystem.Exception;
 
-public class StudentNotFoundException extends RuntimeException {
-  public StudentNotFoundException(String message) {
+public class StudentNotAvailableException extends RuntimeException {
+  public StudentNotAvailableException(String message) {
     super(message);
   }
 }
